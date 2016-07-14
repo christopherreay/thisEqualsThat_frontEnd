3012cbf8ae7a78a9d99e8806b9fcc68de5b8b095
Christopher Reay <christopherreay@gmail.com>
2016-07-14 09:02:19 +0000

    added svgHUD to all rep_ in database. Added optimisation, if degreeOfRandom == 0 do nothing to the three randomiseClones functions

--- javascript/thisEqualsThat.js
+++ javascript/thisEqualsThat.js
@@ -1748,87 +1748,84 @@ $(function(){
 
     var randomiseFunctions = {};
     randomiseFunctions.randomisePosition      = function(degreeOfRandom)
-    { if (degreeOfRandom == 0)
-      { return;
-      }
+    { if (degreeOfRandom != 0)
+      { normalDistribution("randomisePosition", true);
+        $(This.svgHUD.modelInstance.display.svgVisualisationG)
+            .find("g") //cghange to children or correct selector for children
+            .each(  
+                function()
+                { var gBBox = this.getBBox();
+                  var maxXChange = gBBox.width  / 80;
+                  var maxYChange = gBBox.height / 80;
 
-      normalDistribution("randomisePosition", true);
-      $(This.svgHUD.modelInstance.display.svgVisualisationG)
-          .find("g") //cghange to children or correct selector for children
-          .each(  
-              function()
-              { var gBBox = this.getBBox();
-                var maxXChange = gBBox.width  / 80;
-                var maxYChange = gBBox.height / 80;
+                  var changeX = ((normalDistribution("randomisePosition")  * degreeOfRandom * maxXChange) );
+                  var changeY = ((normalDistribution("randomisePosition")  * degreeOfRandom * maxYChange) );
 
-                var changeX = ((normalDistribution("randomisePosition")  * degreeOfRandom * maxXChange) );
-                var changeY = ((normalDistribution("randomisePosition")  * degreeOfRandom * maxYChange) );
+                  var transform;
 
-                var transform;
-
-                if (! this.getAttribute("svgHUD_initial_transform") )
-                { transform = this.getAttribute("transform");
-                  if (transform === null)
-                  { transform = "translate(0 0)";
+                  if (! this.getAttribute("svgHUD_initial_transform") )
+                  { transform = this.getAttribute("transform");
+                    if (transform === null)
+                    { transform = "translate(0 0)";
+                    }
+                    this.setAttribute("svgHUD_initial_transform", transform);
                   }
-                  this.setAttribute("svgHUD_initial_transform", transform);
-                }
-                else
-                { transform = this.getAttribute("svgHUD_initial_transform");
-                }
+                  else
+                  { transform = this.getAttribute("svgHUD_initial_transform");
+                  }
 
-                var translate = transform.match(/^translate\(([-]?\d+[.]?\d*)\s*([-]?\d+[.]?\d*)\)$/);
-                var newX = Number(translate[1]) + changeX;
-                var newY = Number(translate[2]) + changeY;
-                var newTranslate = "translate("+newX+" "+newY+")";
-                this.setAttribute("transform", newTranslate);
-                //console.log(this, transform);
-              }
-          )
+                  var translate = transform.match(/^translate\(([-]?\d+[.]?\d*)\s*([-]?\d+[.]?\d*)\)$/);
+                  var newX = Number(translate[1]) + changeX;
+                  var newY = Number(translate[2]) + changeY;
+                  var newTranslate = "translate("+newX+" "+newY+")";
+                  this.setAttribute("transform", newTranslate);
+                  //console.log(this, transform);
+                }
+            );
+      }
       contextByVisualisation.randomisePosition.degreeOfRandom = degreeOfRandom;
     };
     randomiseFunctions.randomiseColors = function(degreeOfRandom, init=false)
-    { if (degreeOfRandom == 0)
-      { return;
+    { if (degreeOfRandom != 0)
+      { normalDistribution("randomiseColors", true);
+
+        $(This.svgHUD.modelInstance.display.svgVisualisationG)
+            .find("path")
+            .each(  
+                function()
+                { if (this.getAttribute("svgHUD_protectedColor") )
+                  { return;
+                  }
+
+
+                  if (! this.getAttribute("initial_fill_path") )
+                  { this.setAttribute("initial_fill_path", $(this).css("fill") );
+                  }
+                  var colorRGB = this.getAttribute("initial_fill_path");
+                  if (colorRGB == null)
+                    colorRGB = "rgb(50, 50, 50)";
+                  if (colorRGB.indexOf("rgb") === 0)
+                  { var rgb       = colorRGB.match(/^rgb[a]?\((\d+),\s*(\d+),\s*(\d+)[,]?\s*(\d*[.]?\d*)\)$/);
+                    r = Number(rgb[1]);
+                    newR = Math.round(Math.max((((degreeOfRandom * normalDistribution("randomiseColors") * 10.0) - 5) ) + r, 0));
+                    g = Number(rgb[2]);
+                    newG = Math.round(Math.max((((degreeOfRandom * normalDistribution("randomiseColors") * 10.0) - 5) ) + g, 0));
+                    b = Number(rgb[3]);
+                    newB = Math.round(Math.max((((degreeOfRandom * normalDistribution("randomiseColors") * 10.0) - 5) ) + b, 0));
+                    if (4 in rgb && rgb[4] != "")
+                      newRGB = "fill: rgba("+newR+", "+newG+", "+newB+", "+rgb[4]+");";
+                    else
+                      newRGB = "fill: rgb("+newR+", "+newG+", "+newB+");"
+                    //$(this).css("fill", newRGB);
+                    //newStyle = this.getAttribute("style");
+                    //if (newStyle)+newRGB;
+                    this.setAttribute("style", newRGB);
+                    this.setAttribute("initial_fill_group", newRGB.substring(6).replace(";", ""));
+                  }
+                }
+            );
       }
-
-      normalDistribution("randomiseColors", true);
-
-      $(This.svgHUD.modelInstance.display.svgVisualisationG)
-          .find("path")
-          .each(  
-              function()
-              { if (this.getAttribute("svgHUD_protectedColor") )
-                { return;
-                }
-
-
-                if (! this.getAttribute("initial_fill_path") )
-                { this.setAttribute("initial_fill_path", $(this).css("fill") );
-                }
-                var colorRGB = this.getAttribute("initial_fill_path");
-                if (colorRGB == null)
-                  colorRGB = "rgb(50, 50, 50)";
-                if (colorRGB.indexOf("rgb") === 0)
-                { var rgb       = colorRGB.match(/^rgb[a]?\((\d+),\s*(\d+),\s*(\d+)[,]?\s*(\d*[.]?\d*)\)$/);
-                  r = Number(rgb[1]);
-                  newR = Math.round(Math.max((((degreeOfRandom * normalDistribution("randomiseColors") * 10.0) - 5) ) + r, 0));
-                  g = Number(rgb[2]);
-                  newG = Math.round(Math.max((((degreeOfRandom * normalDistribution("randomiseColors") * 10.0) - 5) ) + g, 0));
-                  b = Number(rgb[3]);
-                  newB = Math.round(Math.max((((degreeOfRandom * normalDistribution("randomiseColors") * 10.0) - 5) ) + b, 0));
-                  if (4 in rgb && rgb[4] != "")
-                    newRGB = "fill: rgba("+newR+", "+newG+", "+newB+", "+rgb[4]+");";
-                  else
-                    newRGB = "fill: rgb("+newR+", "+newG+", "+newB+");"
-                  //$(this).css("fill", newRGB);
-                  //newStyle = this.getAttribute("style");
-                  //if (newStyle)+newRGB;
-                  this.setAttribute("style", newRGB);
-                  this.setAttribute("initial_fill_group", newRGB.substring(6).replace(";", ""));
-                }
-              }
-          );
+      
       contextByVisualisation.randomiseColors.degreeOfRandom = degreeOfRandom;
 
       if (!init)
@@ -1836,55 +1833,60 @@ $(function(){
       }
     }
     randomiseFunctions.randomiseColorsByGroup = function(degreeOfRandom, init=false)
-    { if (degreeOfRandom == 0)
-      { return;
-      }
+    { 
+      if (degreeOfRandom != 0)
+      { normalDistribution("randomiseColorsByGroup", true);
+        { $(This.svgHUD.modelInstance.display.svgVisualisationG)
+              .find("g")
+              .each(  
+                  function()
+                  { 
+                      
+                      var changeR = Math.round( 5 *  degreeOfRandom * normalDistribution("randomiseColorsByGroup") );
+                      var changeG = Math.round( 5 *  degreeOfRandom * normalDistribution("randomiseColorsByGroup") );
+                      var changeB = Math.round( 5 *  degreeOfRandom * normalDistribution("randomiseColorsByGroup") );
+                      
+                      //$(this).css("fill", newRGB);
+                      //newStyle = this.getAttribute("style");
+                      //if (newStyle)+newRGB;
+                      $(this)
+                          .find("path")
+                          .each(
+                              function()
+                              { if (this.getAttribute("svgHUD_protectedColor") )
+                                { return;
+                                }
 
-      normalDistribution("randomiseColorsByGroup", true);
-      { $(This.svgHUD.modelInstance.display.svgVisualisationG)
-            .find("g")
-            .each(  
-                function()
-                { 
-                    
-                    var changeR = Math.round( 5 *  degreeOfRandom * normalDistribution("randomiseColorsByGroup") );
-                    var changeG = Math.round( 5 *  degreeOfRandom * normalDistribution("randomiseColorsByGroup") );
-                    var changeB = Math.round( 5 *  degreeOfRandom * normalDistribution("randomiseColorsByGroup") );
-                    
-                    //$(this).css("fill", newRGB);
-                    //newStyle = this.getAttribute("style");
-                    //if (newStyle)+newRGB;
-                    $(this)
-                        .find("path")
-                        .each(
-                            function()
-                            { if (this.getAttribute("svgHUD_protectedColor") )
-                              { return;
+                                if (! this.getAttribute("initial_fill_group") )
+                                { this.setAttribute("initial_fill_group", $(this).css("fill"));
+                                }
+                                var colorRGB = this.getAttribute("initial_fill_group");
+                                
+                                if (colorRGB == null)
+                                  colorRGB = "rgb(50, 50, 50)";
+                                if (colorRGB.indexOf("rgb") === 0)
+                                { var rgb       = colorRGB.match(/^rgb[a]?\((\d+),\s*(\d+),\s*(\d+)[,]?\s*(\d*[.]?\d*)\)$/);
+                                  var r = Number(rgb[1]);
+                                  var newR = Math.max(r + changeR, 0);
+                                  var g = Number(rgb[2]);
+                                  var newG = Math.max(g + changeG, 0);
+                                  var b = Number(rgb[3]);
+                                  var newB = Math.max(b + changeB, 0);
+                                  if (4 in rgb && rgb[4] != "")
+                                    var newRGB = "fill: rgba("+newR+", "+newG+", "+newB+", "+rgb[4]+");";
+                                  else
+                                    var newRGB = "fill: rgb("+newR+", "+newG+", "+newB+");"
+                                  this.setAttribute("style", newRGB);
+                                }
                               }
+                          );
+                  }
+              );
+        }
 
-                              var colorRGB = this.getAttribute("initial_fill_group");
-                              if (colorRGB == null)
-                                colorRGB = "rgb(50, 50, 50)";
-                              if (colorRGB.indexOf("rgb") === 0)
-                              { var rgb       = colorRGB.match(/^rgb[a]?\((\d+),\s*(\d+),\s*(\d+)[,]?\s*(\d*[.]?\d*)\)$/);
-                                var r = Number(rgb[1]);
-                                var newR = Math.max(r + changeR, 0);
-                                var g = Number(rgb[2]);
-                                var newG = Math.max(g + changeG, 0);
-                                var b = Number(rgb[3]);
-                                var newB = Math.max(b + changeB, 0);
-                                if (4 in rgb && rgb[4] != "")
-                                  var newRGB = "fill: rgba("+newR+", "+newG+", "+newB+", "+rgb[4]+");";
-                                else
-                                  var newRGB = "fill: rgb("+newR+", "+newG+", "+newB+");"
-                                this.setAttribute("style", newRGB);
-                              }
-                            }
-                        );
-                }
-            )
+        contextByVisualisation.randomiseColorsByGroup.degreeOfRandom = degreeOfRandom;
       };
-      contextByVisualisation.randomiseColorsByGroup.degreeOfRandom = degreeOfRandom;
+      
     };
     
     var spectrumFunction = function(randomiseItem, itemContext, functionToCall)
