fbb572ef825d06a927a4516d480dec8029502ee6
Christopher Reay <christopherreay@gmail.com>
2017-05-11 19:10:08 +0000

    added google analytics tracking, matching widgets on both urls. Added hotjar tracking for both dev and visualtools. but neither are up on production yet

--- javascript/thisEqualsThat.js
+++ javascript/thisEqualsThat.js
@@ -333,6 +333,9 @@ thisEqualsThat.oop = function()
     { this.currentModelClass.modelInstance.hide();
     }
     this.currentModelClass = modelClass;
+
+    ga('send', 'pageview', "/blueprint/"+modelClass.name);
+
   }
 
 
@@ -3052,7 +3055,26 @@ thisEqualsThat.oop = function()
 
 $().ready(
   function()
-  { window.ThisEqualsThat = new thisEqualsThat.oop();
+  { (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-99026610-1', 'auto');
+    ga('send', 'pageview');
+
+
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:500067,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+
+
+    window.ThisEqualsThat = new thisEqualsThat.oop();
 
     ThisEqualsThat.init();
   }
